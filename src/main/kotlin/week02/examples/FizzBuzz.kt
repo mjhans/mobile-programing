303e2f1042b2a7ed3c23f6fbbe1b5a6b7f6ec787
mjhans <mjhans@krafton.com>
2025-09-11 22:24:15 +0900

    자료 추가

--- src/main/kotlin/week02/examples/FizzBuzz.kt
+++ src/main/kotlin/week02/examples/FizzBuzz.kt
@@ -1,14 +1,5 @@
 package com.bible.week02.examples
 
 fun main(){
-    fun fizzBuzz(i: Int) = when {
-        i % 15 == 0 -> "FizzBuzz "
-        i % 3 == 0 -> "Fizz "
-        i % 5 == 0 -> "Buzz "
-        else -> "$i "
-    }
 
-    for (i in 1..100) {
-        print(fizzBuzz(i))
-    }
 }
